d722c70d3e15ab0f1edbc953b2c34a600e03c82a
Asya <asyabareyan@gmail.com>
2023-01-18 12:23:21 +0400

    add tests

--- itSpaceQuiz-rest/src/test/java/com/example/itspacequizrest/controller/QuizControllerTest.java
+++ itSpaceQuiz-rest/src/test/java/com/example/itspacequizrest/controller/QuizControllerTest.java
@@ -1,38 +1,119 @@
 package com.example.itspacequizrest.controller;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.example.itspacequizcommon.entity.Quiz;
+import com.example.itspacequizcommon.repository.QuizRepository;
+import com.example.itspacequizrest.service.QuizService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.time.LocalDateTime;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+//@TestPropertySource("/application.properties")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class QuizControllerTest {
+    private static MockHttpServletRequest request;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mvc;
+    @Autowired
+    private QuizRepository quizRepository;
+    @InjectMocks
+    private QuizService quizService;
 
     @BeforeEach
-    void setUp() {
-    }
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build();
 
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void saveQuiz() {
     }
 
     @Test
-    void deleteById() {
+    @Order(1)
+    void testAllQuizzes() throws Exception {
+        addTestQuizzes();
+        mvc.perform(MockMvcRequestBuilders.get("/quiz")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+
+        assertEquals(2, quizRepository.findAll().size());
     }
 
-    @Test
-    void getQuizById() {
-    }
 
     @Test
-    void allQuizzes() {
+    @Order(2)
+    void testGetQuizById() throws Exception {
+//        addTestQuizzes();
+
+        int id = 1;
+        mvc.perform(MockMvcRequestBuilders.get("/quiz/{id}", id))
+                .andExpect(status().isOk());
+
+        assertTrue(quizRepository.findById(id).isPresent());
     }
 
+
     @Test
-    void updateQuiz() {
+    @Order(3)
+    void testSaveQuiz() throws Exception {
+        ObjectNode objectNode = new ObjectMapper().createObjectNode();
+        objectNode.put("id", 1);
+        objectNode.put("title", "spring");
+
+        mvc.perform(MockMvcRequestBuilders.post("/quiz")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectNode.toString()))
+                .andExpect(status().isOk());
+
+        assertNotNull(quizRepository.findById(1).isPresent());
+    }
+
+
+    @Test
+    @Order(4)
+    public void delete_quiz() throws Exception {
+        addTestQuizzes();
+        assertTrue((quizRepository.findById(1).isPresent()));
+        mvc.perform(MockMvcRequestBuilders.delete("/quiz/{id}", 1))
+                .andExpect(status().isOk());
+        assertFalse(quizRepository.findById(1).isPresent());
+        mvc.perform(MockMvcRequestBuilders.delete("/quiz/{id}", 1))
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    private void addTestQuizzes() {
+        quizRepository.save(Quiz.builder()
+                .id(1)
+                .title("Quiz1")
+                .createdDateTime(LocalDateTime.now())
+                .build());
+        quizRepository.save(Quiz.builder()
+                .id(2)
+                .title("Quiz2")
+                .createdDateTime(LocalDateTime.now())
+                .build());
+
     }
 }
